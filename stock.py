706083178006bb3ef63ff689e27ad2ac127cbf07
morris <tappanmorris@outlook.com>
2021-01-22 18:31:29 +0800

    find stock price near 50,100,200

--- stock.py
+++ stock.py
@@ -1,53 +1,149 @@
+import os
+import csv
+import time
+import openpyxl
+import numpy as np
 import tushare as ts
+from math import floor
+from pandas.io.parsers import read_csv
+from concurrent.futures import ThreadPoolExecutor,  as_completed
 
-ts.set_token('e0971dcf036c61bd699ac86762b33d9e6d44025979ac91f6dfc58b31')
-pro = ts.pro_api()
-data = pro.stock_basic(exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
-
-
-print(data)
-
-# import baostock as bs
-# import pandas as pd
-
-# #### 登陆系统 ####
-# lg = bs.login()
-# # 显示登陆返回信息
-# print('login respond error_code:'+lg.error_code)
-# print('login respond  error_msg:'+lg.error_msg)
+from tushare import stock
 
 
-# #### 获取历史K线数据 ####
-# # 详细指标参数，参见“历史行情指标参数”章节
-# rs = bs.query_history_k_data_plus("sh.600000",
-#     "date,code,open,high,low,close,preclose,volume,amount,adjustflag,turn,tradestatus,pctChg,peTTM,pbMRQ,psTTM,pcfNcfTTM,isST",
-#     start_date='2021-01-21', end_date='2021-01-21', 
-#     frequency="d", adjustflag="3") #frequency="d"取日k线，adjustflag="3"默认不复权
-# print('query_history_k_data_plus respond error_code:'+rs.error_code)
-# print('query_history_k_data_plus respond  error_msg:'+rs.error_msg)
+def partition(ls, size):
+    """
+    Returns a new list with elements
+    of which is a list of certain size.
 
-# #### 打印结果集 ####
-# data_list = []
-# while (rs.error_code == '0') & rs.next():
-#     # 获取一条记录，将记录合并在一起
-#     data_list.append(rs.get_row_data())
-# result = pd.DataFrame(data_list, columns=rs.fields)
-# #### 结果集输出到csv文件 ####
-# result.to_csv("D:/history_k_data.csv", encoding="gbk", index=False)
-# print(result)
+        >>> partition([1, 2, 3, 4], 3)
+        [[1, 2, 3], [4]]
+    """
+    return [ls[i:i+size] for i in range(0, len(ls), size)]
 
-#### 登出系统 ####
-# bs.logout()
 
-# import math
+class Stock:
+    def __init__(self) -> None:
+        super().__init__()
 
-# def calculate(year):
-#     add = 15
-#     sum = 10 * math.pow(1.5, year)
-#     for i in range(0, year):
-#         sum += add * math.pow(1.5, i)
+        array = [100*n for n in range(1,2)]
+        array.append(50)
+        self.array = array
+        ts.set_token('e0971dcf036c61bd699ac86762b33d9e6d44025979ac91f6dfc58b31')
+        self.pro = ts.pro_api()
+        # data = self.pro.stock_basic(exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
+        # data.to_excel('stock.xlsx')
+        self.pool = ThreadPoolExecutor(128)
+        self.price = list()
+        self.result = list()
 
-#     print(sum)
 
-# calculate(5)
+
+    def read_csv(self):
+        csv_file = open('stock.csv', 'r', encoding='UTF-8')
+        reader = csv.reader(csv_file)
+
+        data = []
+        dic_name = dict()
+        for item in reader:
+            if reader.line_num == 1:
+                continue
+            data.append(item[1])
+            dic_name[item[1]] = item[3]
+
+        csv_file.close()
+        self.data = data
+        self.dic_name = dic_name
+        
+
+    def find_nearest(self, value):
+        array = np.asarray(self.array)
+        idx = (np.abs(array - value)).argmin()
+        return array[idx]
+
+
+    def daily(self,ts_code):
+        date = '20210121'
+        df = self.pro.daily(ts_code=ts_code, start_date=date, end_date=date)
+        # print(df.to_dict())
+
+        # print(df)
+        return df
+
+
+
+    def all(self):
+        n = 50
+        i=0
+        res = partition(self.data, floor(len(self.data)/n))
+        for item in res:
+            i = i+1
+            print("all:", str(i))
+            df = self.daily(','.join(item))
+            self.calcualte(df)
+        
+
+    def calcualte(self, df):  
+        for value, ts_code, change in zip(df.high, df.ts_code, df.change):
+            try:
+                # print(ts_code, value)
+                near_value = self.find_nearest(value)
+                if value>=near_value*0.93 and value<=near_value*1.07:
+                    if change > 0:
+                        self.result.append(ts_code)
+            except:
+                pass
+
+            
+        # print(self.result)
+
+
+    def add_name(self):
+        openpyxl.load_workbook('stock.xlsx')
+        stock_wb =  openpyxl.load_workbook(filename = 'stock.xlsx')
+        stock_source = stock_wb['Sheet1']
+        dic = dict()
+        for row in range(1,stock_source.max_row):
+            key = stock_source.cell(row=row, column=2).value
+            value = stock_source.cell(row=row, column=4).value
+            dic[key] = value
+
+        
+        daily_wb = openpyxl.load_workbook(filename='0.xlsx')
+        daily_source = daily_wb['Sheet1']
+        for row in range(2, 65):
+            key = daily_source.cell(row=row, column=2).value
+            daily_source.cell(row=row, column=13, value=dic[key])
+
+        daily_wb.save(filename='0.xlsx')   
+
+        
+
+
+
+def run():
+
+    time_start = time.time()
+
+    stock = Stock()
+    # stock.read_csv()
+    # stock.all()
+    # print('len: ' + str(len(stock.result)))
+    # num = floor(len(stock.result)/100)+1
+    # res = partition(stock.result, floor(len(stock.result)/num))
+    # for i, item in zip(range(num),res):
+    #     df = stock.daily(','.join(item))
+    #     df.to_excel(str(i)+'.xlsx')
+
+    stock.add_name()
+    
+    
+    time_end = time.time()
+    print('time cost', time_end-time_start, 's')
+
+    print('finish')
+    # os.system('pause')
+
+
+run()
 
